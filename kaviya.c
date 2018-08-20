a33bdcb3288049052e41dd5dd4cafe6485bf490c
mohanakalai <36880272+mohanakalai@users.noreply.github.com>
2018-08-20 12:09:16 +0530

    Create kaviya.c

---
+++ kaviya.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n;
+printf("enter a number");
+scanf("%d",n);
+if(n<0)
+{
+printf("the number is zero")
+}
+else(n>0)
+printf("the number is positive")
+}
+else printf("the number is negative")
+getch();
+}
